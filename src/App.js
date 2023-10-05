e4ab28fb72d055f9ba5903c432209a955d83b697
David Stein <dstein131@gmail.com>
2023-10-05 10:49:00 -0400

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+// App.js
+
+import React, { useState } from 'react';
 import './App.css';
 
+// This is a React functional component.
 function App() {
+  // Using the useState hook to create a piece of state to hold comments.
+  const [comments, setComments] = useState([]);
+  const [input, setInput] = useState('');
+
+  // This function will be triggered when submitting a new comment.
+  const handleCommentSubmit = (e) => {
+    e.preventDefault();
+
+    // Update the comments state by appending the new comment.
+    setComments([...comments, input]);
+    setInput('');  // Clear the input field.
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Teaching Demo</h1>
+
+      {/* JSX syntax, allowing for a mix of JS and HTML. */}
+      <form onSubmit={handleCommentSubmit}>
+        <input 
+          value={input} 
+          onChange={(e) => setInput(e.target.value)} 
+          placeholder="Add a comment"
+        />
+        <button type="submit">Submit</button>
+      </form>
+
+      <ul>
+        {/* Rendering a list of comments using .map */}
+        {comments.map((comment, index) => (
+          <li key={index}>{comment}</li>
+        ))}
+      </ul>
     </div>
   );
 }
